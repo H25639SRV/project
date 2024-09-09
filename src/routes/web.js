d551c526ef6c68e4ea223dfe85bbe1ff92a4d566
H25639SRV <giahuytruong2506@gmail.com>
2024-09-09 20:51:09 +0700

    init project

---
+++ src/routes/web.js
@@ -0,0 +1,18 @@
+import express from "express";
+import homeController from "../controller/homeController.js";
+
+const router = express.Router();
+
+/**
+ * @param {*} app : express app
+ */
+
+const initWebRoutes = (app) => {
+  //path, handlers
+  router.get("/", homeController.handleHelloWorld);
+  router.get("/user", homeController.handleUserPage);
+
+  return app.use("/", router);
+};
+
+export default initWebRoutes;
