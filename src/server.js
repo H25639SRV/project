bdca63993e8d3a0ebf3f61287770b7fe890bb8ea
H25639SRV <giahuytruong2506@gmail.com>
2024-09-13 23:07:19 +0700

    orm structure

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import configViewEngine from "./config/viewEngine";
 import initWebRoutes from "./routes/web";
 require("dotenv").config();
 import bodyParser from "body-parser";
+// import connection from "./config/connectDB.js";
 const app = express();
 
 //configViewEngine
@@ -11,6 +12,9 @@ configViewEngine(app);
 //config body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//test connection
+// connection();
 
 //initWebRoutes
 initWebRoutes(app);
