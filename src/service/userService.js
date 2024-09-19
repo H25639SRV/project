4b07e89e097c16dca88dcf5280cd72d8c0538801
H25639SRV <giahuytruong2506@gmail.com>
2024-09-19 20:15:25 +0700

    models relationship

--- src/service/userService.js
+++ src/service/userService.js
@@ -37,6 +37,25 @@ const createNewUser = async (email, password, username) => {
 };
 
 const getUserList = async () => {
+
+  //test relationship
+  let newUser = await db.User.findOne({
+    where: {id: 1},
+    attributes: ["id", "username", "email"],
+    include: {model: db.Group, attributes: ["name", "description"]},
+    raw: true,
+    nest: true
+  })
+  console.log(">>>new user: ", newUser)
+
+
+  let newRole = await db.Role.findAll({
+    include: {model: db.Group, where: {id: 1} },
+    raw: true,
+    nest: true
+  })
+  console.log(">>>new roles: ", newRole)
+
   let users = [];
   users = await db.User.findAll();  
   return users;
